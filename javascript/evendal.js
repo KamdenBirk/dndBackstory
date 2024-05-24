4abab4197aa9733f582d730c5bf9c1c64f3eb77c
Kamden <birkkamden@gmail.com>
2024-05-24 16:22:42 -0500

    added descriptions for features

---
+++ javascript/evendal.js
@@ -0,0 +1,17 @@
+const featureList = document.getElementsByClassName("features");
+
+document.getElementById("arcaneRecovery").addEventListener("click", function() {
+    alert("You have learned to regain some of your magical energy by studying your spellbook. Once per day when you finish a short rest, you can choose expended spell slots to recover. The spell slots can have a combined level that is equal to or less than half your wizard level (rounded up), and none of the slots can be 6th level or higher. For example, if you're a 4th-level wizard, you can recover up to two levels worth of spell slots. You can recover either a 2nd-level spell slot or two 1st-level spell slots.");
+});
+
+document.getElementById("wizardQuill").addEventListener("click", function() {
+    alert("At 2nd level, as a bonus action, you can magically create a Tiny quill in your free hand. The magic quill has the following properties: The quill doesn't require ink. When you write with it, it produces ink in a color of your choice on the writing surface. The time you must spend to copy a spell into your spell book equals 2 minutes per spell level if you use the quill for the transcription. You can erase anything you write with the quill if you wave the feather over the text as a bonus action, provided the text is within 5 feet of you. This quill disappears if you create another one or if you die.");
+});
+
+document.getElementById("awakenedSpellbook").addEventListener("click", function() {
+    alert("Using specially prepared inks and ancient incantations passed down by your wizardly order, you have awakened an arcane sentience within your spellbook. At 2nd level, while you are holding the book, it grants you the following benefits: You can use the book as a spellcasting focus for your wizard spells. When you cast a wizard spell with a spell slot, you can temporarily replace its damage type with a type that appears in another spell in your spellbook, which magically alters the spell's formula for this casting only. The latter spell must be of the same level as the spell slot you expend. When you cast a wizard spell as a ritual, you can use the spell's normal casting time, rather than adding 10 minutes to it. Once you use this benefit, you can't do so again until you finish a long rest. If necessary, you can replace the book over the course of a short rest by using your Wizardly Quill to write arcane sigils in a blank book or a magic spellbook to which you're attuned. At the end of the rest, your spellbook's consciousness is summoned into the new book, which the consciousness transforms into your spellbook, along with all its spells. If the previous book still existed somewhere, all the spells vanish from its pages.");
+});
+
+document.getElementById("researcher").addEventListener("click", function() {
+    alert("When you attempt to learn or recall a piece of lore, if you do not know that information, you often know where and from whom you can obtain it. Usually, this information comes from a library, scriptorium, university, or a sage or other learned person or creature. Your DM might rule that the knowledge you seek is secreted away in an almost inaccessible place, or that it simply cannot be found. Unearthing the deepest secrets of the multiverse can require an adventure or even a whole campaign.");
+});
